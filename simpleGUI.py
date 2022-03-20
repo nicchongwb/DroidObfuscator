4969db3b00952f3f443890ed4d727fc1bc120ff7
Xavier Lim <xav2601@gmail.com>
2022-03-20 13:19:46 +0800

    Update simpleGUI.py
    Bug fixed for path directory in windows

--- simpleGUI.py
+++ simpleGUI.py
@@ -89,17 +89,18 @@ def Func3():
 
 def repackage():
     '''Rebuilds and resigns the apk from the smali files'''
-    '''targetFile = simpledialog.askstring("Target File to recompile & sign", "What is the name of the folder to recompile? \nNote: Ensure exe is in the same level directory as folder.")'''
-    '''if targetFile is None:'''
-    '''    return	     '''
+    #targetFile = simpledialog.askstring("Target File to recompile & sign", "What is the name of the folder to recompile? \nNote: Ensure exe is in the same level directory as folder.")
 
-    targetFile = filedialog.askdirectory(initialdir=os.getcwd(), title="Select your Source File directory") #Opens file prompt for input
+    #if targetFile is None:
+    #    return
+    targetFile = filedialog.askdirectory(initialdir=os.getcwd(), title="Select your Source File directory")
           
     if (targetFile == "" or targetFile is None):
         return
     
+    fullPath = targetFile #Full path to the file
     targetDir = os.path.dirname(targetFile) #Checks only for relevant files and folder in the directory
-    targetFile = os.path.basename(os.path.normpath(targetFile)) #Strips path to retrieve the target folder
+    targetFile = os.path.basename(os.path.normpath(targetFile)) #Folder Name
     searchResult = None #Used to search for the relevant files in current directory
     fileList = os.listdir(targetDir)
     if (targetFile not in fileList):
@@ -112,9 +113,9 @@ def repackage():
     if (targetFile not in fileList):
         messagebox.showerror(title="Error", message=str(targetFile) + " not found in current directory!")
         return
-
+    
     fileName = "apktool"
-
+    
     for file in fileList:
         if (fileName in file):
             searchResult = file
@@ -127,16 +128,16 @@ def repackage():
         subprocess.run(["java", "-jar", searchResult, "b", targetFile]) #Recompiles back to APK
 
     searchResult = None #Clears search
-
+    
     for file in fileList:
             if (file.endswith(".jks")): #Searches for a key
                 searchResult = file
-
+                
     key = searchResult
     searchResult = None #Clears search
-
-    if (os.name == "posix"): #Only run in linux to sign the app
-
+    
+    if (os.name == "posix"): #Only run in linux to sign the app   
+        
         if(key == None):
             print("ERROR SIGNING! Key not found!")
             messagebox.showerror(title="Error", message="apktool file not found in same directory as this exe!") #error message if no key is found
@@ -147,30 +148,31 @@ def repackage():
         else:
             print("Key Found! Using " + searchResult + "to sign app")
             messagebox.showinfo("Key Found","Key Found! Using " + searchResult + "to sign + " + targetFile + ".apk")
-            subprocess.run(["jarsigner", "-verbose", "-sigalg", "SHA1withRSA", "-digestalg", "SHA1", "-keystore", searchResult, targetFile +".apk", "2207"])
+            subprocess.run(["jarsigner", "-verbose", "-sigalg", "SHA1withRSA", "-digestalg", "SHA1", "-keystore", searchResult, targetFile +"/dist/" + targetFile +".apk", "2207"])
             subprocess.run(["jarsigner", "-verify", "-verbose", "-certs", targetFile + ".apk"])
         return
 
     elif (os.name == "nt"): #For those clients running in Windows to sign the app
-        targetDir = "C:/Program Files/Java/" #Search for both keytool and jarsigner in the JDK directory, searches for any version of JDK
-        fileList = os.listdir(targetDir)
+        targetJavaDir = "C:/Program Files/Java/" #Search for both keytool and jarsigner in the JDK directory, searches for any version of JDK
+        fileList = os.listdir(targetJavaDir)
         check = 0
+        print(targetFile)
         for i in fileList: #Search for bin directory on Windows as the two binaries requires relevant dll to function
             if(i.startswith("jdk")):
-                targetDir = targetDir + i + "/bin/"
-                fileList = os.listdir(targetDir)
+                targetJavaDir = targetJavaDir + i + "/bin/"
+                fileList = os.listdir(targetJavaDir)
                 check = 1 #Hit found for JDK
-
+               
         if(check == 0):
             messagebox.showerror(title="Error", message="Unable to locate JDK directory! Ensure JDK is in C:\Program Files\Java!")
             return
-
+                
         if(key == None): #Unable to find key in directory, using keytool to create
             messagebox.showerror(title="Error", message="Key with .jks extension not found! Press OK to create a key!")
             fileName = "keytool.exe"
-
+            
             if (fileName in fileList):
-                searchResult = targetDir + fileName
+                searchResult = targetJavaDir + fileName
                 subprocess.run([searchResult, "-genkey", "-v", "-keystore", "2207RSAKey.jks", "-keyalg", "RSA", "-keysize", "2048","-validity", "10000", "-alias", "2207"])
                 key = "2207RSAKey"
             else:
@@ -178,20 +180,22 @@ def repackage():
                 return
         else:
             messagebox.showinfo("Key Found","Key Found! Using " + key + " to sign " + targetFile + ".apk")
-
+        
         searchResult = None
-
+        
         fileName = "jarsigner.exe"
         if (fileName in fileList):
-            searchResult = targetDir + fileName
-
+            searchResult = targetJavaDir + fileName
+            
         if(searchResult != None):
-            subprocess.run([searchResult, "-verbose", "-sigalg", "SHA1withRSA", "-digestalg", "SHA1", "-keystore", key, targetFile +".apk", "2207"])
+            subprocess.run([searchResult, "-verbose", "-sigalg", "SHA1withRSA", "-digestalg", "SHA1", "-keystore", key, fullPath + "/dist/" + targetFile +".apk", "2207"])
             subprocess.run([searchResult, "-verify", "-verbose", "-certs", targetFile + ".apk"])
+            print("Check dist directory in " + targetFile + " folder whether " + targetFile + " APK is recompiled and signed")
+            ws.lift() #Brings the tkinter window back to front
         else:
             messagebox.showerror(title="Error", message="Unable to locate jarsigner.exe! Ensure JDK is installed in C:\Program Files\Java!")
             return
-
+        
         return #Completes after signing app
 
 ws = Tk()
