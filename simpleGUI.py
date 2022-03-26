f7980963deba88062f1a38bfbc2464f68d85ca2d
vizierofhope <limyongan8888@gmail.com>
2022-03-26 14:33:56 +0800

    Split repackage into 2 functions

--- simpleGUI.py
+++ simpleGUI.py
@@ -125,8 +125,9 @@ def Obfuscate(get_lines):
     except NameError:
         messagebox.showinfo(title="Error", message="Please select a file")
 
+'''
 def repackage():
-    '''Rebuilds and resigns the apk from the smali files'''
+    #Rebuilds and resigns the apk from the smali files
     #targetFile = simpledialog.askstring("Target File to recompile & sign", "What is the name of the folder to recompile? \nNote: Ensure exe is in the same level directory as folder.")
 
     #if targetFile is None:
@@ -235,6 +236,142 @@ def repackage():
             return
 
         return #Completes after signing app
+'''
+
+def recompile():
+    #Rebuilds and resigns the apk from the smali files
+    #targetFile = simpledialog.askstring("Target File to recompile & sign", "What is the name of the folder to recompile? \nNote: Ensure exe is in the same level directory as folder.")
+
+    #if targetFile is None:
+    #    return
+    targetFile = filedialog.askdirectory(initialdir=os.getcwd(), title="Select your Source File directory")
+
+    if (targetFile == "" or targetFile is None):
+        return
+
+    fullPath = targetFile #Full path to the file
+    targetDir = os.path.dirname(targetFile) #Checks only for relevant files and folder in the directory
+    targetFile = os.path.basename(os.path.normpath(targetFile)) #Folder Name
+    searchResult = None #Used to search for the relevant files in current directory
+    fileList = os.listdir(targetDir)
+    if (targetFile not in fileList):
+        messagebox.showerror(title="Error", message=str(targetFile) + " not found in current directory!")
+        return
+
+    searchResult = None #Used to search for the relevant files in current directory
+    targetDir = os.getcwd()  #Checks only for relevant files and folder in current directory
+    fileList = os.listdir(targetDir)
+    if (targetFile not in fileList):
+        messagebox.showerror(title="Error", message=str(targetFile) + " not found in current directory!")
+        return
+
+    fileName = "apktool"
+
+    for file in fileList:
+        if (fileName in file):
+            searchResult = file
+    if(searchResult == None):
+        print("ERROR COMPILING! apktool not found in the same directory as this program!")
+        #Error Message Prompt if apktool is not found
+        messagebox.showerror(title="Error", message="apktool file not found in same directory as this exe!")
+        return
+    else:
+        subprocess.run(["java", "-jar", searchResult, "b", targetFile]) #Recompiles back to APK
+        messagebox.showinfo(title="Completed", message="Built APK")
+        
+def sign():
+     #Rebuilds and resigns the apk from the smali files
+    #targetFile = simpledialog.askstring("Target File to recompile & sign", "What is the name of the folder to recompile? \nNote: Ensure exe is in the same level directory as folder.")
+
+    #if targetFile is None:
+    #    return
+    targetFile = filedialog.askdirectory(initialdir=os.getcwd(), title="Select your Source File directory")
+
+    if (targetFile == "" or targetFile is None):
+        return
+
+    fullPath = targetFile #Full path to the file
+    targetDir = os.path.dirname(targetFile) #Checks only for relevant files and folder in the directory
+    targetFile = os.path.basename(os.path.normpath(targetFile)) #Folder Name
+    searchResult = None #Used to search for the relevant files in current directory
+    fileList = os.listdir(targetDir)
+    if (targetFile not in fileList):
+        messagebox.showerror(title="Error", message=str(targetFile) + " not found in current directory!")
+        return
+        
+    fullPath = targetFile #Full path to the file
+    targetDir = os.path.dirname(targetFile) #Checks only for relevant files and folder in the directory
+    targetFile = os.path.basename(os.path.normpath(targetFile)) #Folder Name
+
+    for file in fileList:
+        if (file.endswith(".jks")): #Searches for a key
+            searchResult = file
+
+    key = searchResult
+    #searchResult = None #Clears search
+
+    if (os.name == "posix"): #Only run in linux to sign the app
+
+        if(key == None):
+            print("ERROR SIGNING! Key not found!")
+            messagebox.showerror(title="Error", message="apktool file not found in same directory as this exe!") #error message if no key is found
+            #Following code spawns shell and prompts user to create a key before signing the APK
+            subprocess.run(["keytool", "-genkey", "-v", "-keystore", "2207RSAKey.jks", "-keyalg", "RSA", "-keysize", "2048","-validity", "10000", "-alias", "2207"])
+            subprocess.run(["jarsigner", "-verbose", "-sigalg", "SHA1withRSA", "-digestalg", "SHA1", "-keystore", "2207RSAKey.jks", targetFile +".apk", "2207"])
+            subprocess.run(["jarsigner", "-verify", "-verbose", "-certs", targetFile + ".apk"])
+        else:
+            print("Key Found! Using " + searchResult + "to sign app")
+            messagebox.showinfo("Key Found","Key Found! Using " + searchResult + "to sign + " + targetFile + ".apk")
+            subprocess.run(["jarsigner", "-verbose", "-sigalg", "SHA1withRSA", "-digestalg", "SHA1", "-keystore", searchResult, targetFile +"/dist/" + targetFile +".apk", "2207"])
+            subprocess.run(["jarsigner", "-verify", "-verbose", "-certs", targetFile + ".apk"])
+        return
+
+    elif (os.name == "nt"): #For those clients running in Windows to sign the app
+        targetJavaDir = "C:/Program Files/Java/" #Search for both keytool and jarsigner in the JDK directory, searches for any version of JDK
+        fileList = os.listdir(targetJavaDir)
+        check = 0
+
+        for i in fileList: #Search for bin directory on Windows as the two binaries requires relevant dll to function
+            if(i.startswith("jdk")):
+                targetJavaDir = targetJavaDir + i + "/bin/"
+                fileList = os.listdir(targetJavaDir)
+                check = 1 #Hit found for JDK
+
+        if(check == 0):
+            messagebox.showerror(title="Error", message="Unable to locate JDK directory! Ensure JDK is in C:\Program Files\Java!")
+            return
+
+        if(key == None): #Unable to find key in directory, using keytool to create
+            messagebox.showerror(title="Error", message="Key with .jks extension not found! Press OK to create a key!")
+            fileName = "keytool.exe"
+
+            if (fileName in fileList):
+                searchResult = targetJavaDir + fileName
+                subprocess.run([searchResult, "-genkey", "-v", "-keystore", "2207RSAKey.jks", "-keyalg", "RSA", "-keysize", "2048","-validity", "10000", "-alias", "2207"])
+                key = "2207RSAKey.jks"
+            else:
+                messagebox.showerror(title="Error", message="Unable to locate keytool.exe! Ensure JDK is installed in C:\Program Files\Java\!")
+                return
+        else:
+            messagebox.showinfo("Key Found","Key Found! Using " + key + " to sign " + targetFile + ".apk")
+
+        searchResult = None
+
+        fileName = "jarsigner.exe"
+        if (fileName in fileList):
+            searchResult = targetJavaDir + fileName
+
+        if(searchResult != None):
+            subprocess.run([searchResult, "-verbose", "-sigalg", "SHA1withRSA", "-digestalg", "SHA1", "-keystore", key, fullPath + "/dist/" + targetFile +".apk", "2207"])
+            subprocess.run([searchResult, "-verify", "-verbose", "-certs", targetFile + ".apk"])
+            print("Check dist directory in " + targetFile + " folder whether " + targetFile + " APK is recompiled and signed")
+            messagebox.showerror(title="Completed", message="Signed")
+            ws.lift() #Brings the tkinter window back to front
+        else:
+            messagebox.showerror(title="Error", message="Unable to locate jarsigner.exe! Ensure JDK is installed in C:\Program Files\Java!")
+            return
+
+        return #Completes after signing app
 
 ws = Tk()
 ws.title("Obfuscation GUI")
@@ -304,19 +441,25 @@ Button(
     ws,
     text="Decompile APK",
     command=lambda: decompile()
-    ).place(x=290, y=550, height=30, width=150)
+    ).place(x=180, y=550, height=30, width=150)
 
 Button(
     ws,
-    text="Recompile & Sign to APK",
-    command=lambda: repackage()
+    text="Sign",
+    command=lambda: sign()
     ).place(x=620, y=500, height=30, width=150)
+    
+Button(
+    ws,
+    text="Recompile APK",
+    command=lambda: recompile()
+    ).place(x=400, y=550, height=30, width=150)
 
 Button(
     ws,
     text="Exit",
     command=lambda:ws.destroy()
-    ).place(x=510, y=550, height=30, width=150)
+    ).place(x=620, y=550, height=30, width=150)
 
 # adding graceful exit due to keyboard interrupts
 try:
